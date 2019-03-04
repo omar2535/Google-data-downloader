c23012e97e28f47a2fba76ec38f7a38e4ba94cae
Omar <omar2535@gmail.com>
2019-03-03 19:14:25 -0800

    adding get attachments

--- gmail/message_utils.py
+++ gmail/message_utils.py
@@ -98,3 +98,30 @@ class MessageUtils:
         except errors.HttpError as error:
             print('An error occurred: ' + error)
     
+    def get_attachments_from_message(self, service, user_id, msg_id, store_dir):
+        """Get and store attachment from Message with given id.
+
+        Args:
+            service: Authorized Gmail API service instance.
+            user_id: User's email address. The special value "me"
+            can be used to indicate the authenticated user.
+            msg_id: ID of Message containing attachment.
+            store_dir: The directory used to store attachments.
+        """
+        try:
+            message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+
+            for part in message['payload']['parts']:
+                if(part['filename']):
+
+                    file_data = base64.urlsafe_b64decode(part['body']['data']
+                                                        .encode('UTF-8'))
+
+                    path = ''.join([store_dir, part['filename']])
+
+                    f = open(path, 'w')
+                    f.write(file_data)
+                    f.close()
+
+        except errors.HttpError as error:
+            print('An error occurred: ' + error)
